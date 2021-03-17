00687c09a21acdfc3cfe55adbb478cce2ae9706e
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2021-03-17 15:00:51 +0700

    initial component post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PostComponent} from './post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'post',
+    component: PostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
