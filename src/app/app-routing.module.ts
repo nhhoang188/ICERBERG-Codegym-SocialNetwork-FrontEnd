c299a5d268c0631da69d1351eafb99736117c739
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2021-03-19 15:57:26 +0700

    nothing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,29 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {PostComponent} from './post/post.component';
-import {HomeComponent} from './home/home.component';
-import {ProfileComponent} from './profile/profile-user/profile.component';
-import {FriendComponent} from './friendrequest/friend/friend.component';
+import {CreatePostComponent} from "./post/create-post/create-post.component";
+import {EditPostComponent} from "./post/edit-post/edit-post.component";
 import {LoginComponent} from './login/login/login.component';
+import {HomeComponent} from './home/home.component';
 import {LogoutComponent} from './login/logout/logout.component';
-import {LoveComponent} from './like/love/love.component';
+import {ProfileComponent} from './profile/profile-user/profile.component';
 
 const routes: Routes = [
-  {path: 'friend', component: FriendComponent},
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
   {path: 'login', component: LoginComponent, pathMatch: 'full'},
   {path: 'home', component: HomeComponent, pathMatch: 'full'},
-  {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'logout', component: LogoutComponent, pathMatch: 'full'},
   {path: 'profile/:id', component: ProfileComponent, pathMatch: 'full'},
-  {path: 'love', component: LoveComponent, pathMatch: 'full'},
   {
-    path: 'post',
-    component: PostComponent
+    path: 'create-post',
+    component: CreatePostComponent
+  },
+  {
+    path: 'edit-post/:id',
+    component: EditPostComponent
   }
 ];
 
