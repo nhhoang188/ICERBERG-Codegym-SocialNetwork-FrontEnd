23def039912f2e7bab8dadd24e2a1fb95026ba20
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2021-03-19 15:50:36 +0700

    post mới

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 import {FriendrequestService} from './services/friendrequest.service';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { LogoutComponent } from './login/logout/logout.component';
+import {HomeComponent} from './home/home.component';
+import {LogoutComponent} from './login/logout/logout.component';
 import {PostComponent} from "./post/post.component";
-import { FriendComponent } from './friendrequest/friend/friend.component';
-import { LoveComponent } from './like/love/love.component';
-import { ProfileComponent } from './profile/profile-user/profile.component';
+import {FriendComponent} from './friendrequest/friend/friend.component';
+import {LoveComponent} from './like/love/love.component';
 import {LoginComponent} from './login/login/login.component';
 import {LoveService} from './services/love.service';
-import { HeaderComponent } from './layout/header/header.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {ProfileComponent} from "./profile/profile-user/profile.component";
 
 @NgModule({
   declarations: [
@@ -37,7 +37,8 @@ import { HeaderComponent } from './layout/header/header.component';
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [FriendrequestService,LoveService],
+  providers: [FriendrequestService, LoveService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
