89d580a5eeabad10ea131d278964b8201decd8ab
trịnh quốc dũng <quockunss@gmail.com>
2021-03-24 14:48:32 +0700

    done list post friend

--- src/app/post/friend-post/friend-post.component.ts
+++ src/app/post/friend-post/friend-post.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../../model/User";
 import {UserService} from "../../services/user.service";
 import {PostService} from "../../services/post.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FriendrequestService} from "../../services/friendrequest.service";
 
 @Component({
   selector: 'app-friend-post',
@@ -12,23 +13,56 @@ import {Router} from "@angular/router";
 export class FriendPostComponent implements OnInit {
   user: User = {};
   listPost: any;
-  userId: any;
+  userId1: any;
+  userId2: any;
+  checkFriend: any;
 
   constructor(private userSv: UserService,
               private postSv: PostService,
+              private friendSv: FriendrequestService,
+              private activeRoute: ActivatedRoute,
               private route: Router) {
-    this.userId = localStorage.getItem('ID');
-    this.userSv.getById(this.userId).subscribe(value => {
+    this.userId1 = localStorage.getItem('ID');
+    console.log("id nguoi dung" + this.userId1);
+    this.userSv.getById(this.userId1).subscribe(value => {
       this.user = value;
+      this.activeRoute.paramMap.subscribe(value => {
+        this.userId2 = value.get('userId');
+        console.log(this.userId2)
+        this.friendSv.getFriend(this.userId1, this.userId2).subscribe(value => {
+          if (value == null) {
+            this.checkFriend = false;
+          } else {
+            this.checkFriend = value.stt;
+          }
+          this.checkFri();
+        }, error => console.log(error))
+      });
     });
-    this.getPublicAndFriendOnlyPostByUserId();
   }
 
+  checkFri() {
+    if (this.checkFriend == true) {
+      this.getPublicAndFriendOnlyPostByUserId();
+    }
+    if (this.checkFriend == false) {
+      this.getPublicPostByUserId();
+    } else {
+      console.log("ko co bai viet");
+    }
+  }
   ngOnInit(): void {
   }
 
-  getPublicAndFriendOnlyPostByUserId(){
-    this.postSv.findPublicAndFriendOnlyPostByUserId(this.userId).subscribe(value => {
+
+  getPublicAndFriendOnlyPostByUserId() {
+    this.postSv.findPublicAndFriendOnlyPostByUserId(this.userId2).subscribe(value => {
+      this.listPost = value;
+    });
+  }
+
+  getPublicPostByUserId() {
+    this.postSv.findPublicPostByUserId(this.userId2).subscribe(value => {
       this.listPost = value;
     });
   }
