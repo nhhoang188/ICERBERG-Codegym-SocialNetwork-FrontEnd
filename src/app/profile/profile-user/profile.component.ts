f9fffb712b8091a7c30e87bb5d9f58cbda01242b
nhhoang188@gmail.com <ghnhh18896>
2021-03-26 09:08:34 +0700

    all image

--- src/app/profile/profile-user/profile.component.ts
+++ src/app/profile/profile-user/profile.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../../services/user.service';
+import {PostService} from '../../services/post.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,10 +12,11 @@ export class ProfileComponent implements OnInit {
   user: any;
   userPublic: any;
   guest: any;
-
+  listPost: any;
   constructor(private router: Router,
               private userSv: UserService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private postSv: PostService) {
   }
 
   ngOnInit(): void {
@@ -23,6 +25,9 @@ export class ProfileComponent implements OnInit {
       if (localStorage.getItem('USERNAME') != null) {
         this.userSv.getById(id).subscribe(value1 => {
           this.user = value1;
+          this.postSv.findPostByUserId(this.user.id).subscribe(value => {
+            this.listPost = value;
+          });
         });
       } else {
         this.userSv.getByIdAndInfoPublic(id).subscribe(value1 => {
@@ -31,6 +36,5 @@ export class ProfileComponent implements OnInit {
       }
     });
   }
-
 
 }
