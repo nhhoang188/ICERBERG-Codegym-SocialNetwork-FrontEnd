9e7111ec784e8edc194841121b17de72b08b5e42
nhhoang188@gmail.com <ghnhh18896>
2021-03-26 10:49:22 +0700

    deo code nua

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CommentService} from '../services/comment.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Comments} from "../model/Comments";
+import {Comments} from '../model/Comments';
 
 
 @Component({
